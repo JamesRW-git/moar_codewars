ce6fc0329a6137ed85d68403d29dc668c079507c
James Woodward <jamesrobertwoodward@gmail.com>
2022-08-04 19:43:10 -0500

    complete min/max exercise

--- Kata.java
+++ Kata.java
@@ -15,14 +15,27 @@
 //    }
 //}
 
+import java.util.Arrays;
+
 public class Kata {
     public static String findNeedle(Object[] haystack) {
         for (int i = 0; i < haystack.length; i++) {
-            if(haystack[i] == "needle") {
+            if (haystack[i] == "needle") {
                 return "found the needle at position " + i;
             }
         }
         return "needle not found";
     }
+
+    public int min(int[] list) {
+        Arrays.sort(list);
+        return list[0];
+    }
+
+    public int max(int[] list) {
+        Arrays.sort(list);
+        return list[list.length - 1];
+    }
 }
+
 
